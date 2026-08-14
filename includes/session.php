73e7fbc7f08d9c1570d44d640f94c24afc60d16b
mahin-sketch11 <mahinfoysal1104@gmail.com>
2026-08-14 21:37:22 +0600

    session.php is modified

--- includes/session.php
+++ includes/session.php
@@ -1,41 +1,25 @@
 <?php
 
-session_start();
-
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 // Check if user is logged in
-
 if (!isset($_SESSION['user_id'])) {
-
-    header("Location: ../login.php");
+    header("Location: /login.php");
     exit();
-
 }
-
 
 // Student Page Protection
-
-if (
-    strpos($_SERVER['PHP_SELF'], "/student/") !== false &&
-    $_SESSION['role'] !== "student"
-) {
-
+if (strpos($_SERVER['PHP_SELF'], "/student/") !== false && $_SESSION['role'] !== "student") {
     header("Location: ../login.php");
     exit();
-
 }
 
-
 // Faculty Page Protection
-
-if (
-    strpos($_SERVER['PHP_SELF'], "/faculty/") !== false &&
-    $_SESSION['role'] !== "faculty"
-) {
-
+if (strpos($_SERVER['PHP_SELF'], "/faculty/") !== false && $_SESSION['role'] !== "faculty") {
     header("Location: ../login.php");
     exit();
-
 }
 
 ?>
